4a5a83c5ef3505a9b98708963418d38a02beb427
mohammadadel4040-svg <mohammadadel4040@gmail.com>
2026-02-27 23:32:57 +0100

    Initialize FastAPI with user management routes

--- week13/main.py
+++ week13/main.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI
+from routes import users
 
+app = FastAPI(
+    title="User Management API",
+    version="1.0"
+)
+
+# connect users routes
+app.include_router(
+    users.router,
+    prefix="/users",
+    tags=["Users"]
+)
+
+
+@app.get("/")
+def root():
+    return {"message": "API is running"}
